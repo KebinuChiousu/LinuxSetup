d3f3a0a11bc18d6fb518e8bc1d24db8542fe1147
Kevin Meredith <kevin@meredithkm.info>
2018-08-14 16:39:32 -0400

    update env scripts

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,6 +1,17 @@
+git_add_modified() {
+  for i in `git status | grep modified | awk '{print $2}'`
+  do
+    echo $i
+    git add $i
+  done
+}
+
+alias gam='git_add_modified'
+alias gbw='git branch working'
 alias gcm='git checkout master'
 alias gcw='git checkout working'
 alias gmm='git merge master'
 alias gmw='git merge working'
 alias gca='git commit -C HEAD --amend'
 alias grh='git reset --hard HEAD'
+
