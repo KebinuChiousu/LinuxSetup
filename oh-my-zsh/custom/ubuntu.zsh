5182a1f72a2dd3dd09feedb7ac7f12acba8662ed
Kevin Meredith <kevin@meredithkm.info>
2018-08-14 16:39:32 -0400

    update env scripts

--- oh-my-zsh/custom/ubuntu.zsh
+++ oh-my-zsh/custom/ubuntu.zsh
@@ -1,1 +1,11 @@
 alias tz='sudo dpkg-reconfigure tzdata'
+alias idg="id | tr ',' '\n'"
+alias du-nc="du --exclude=.cache -h"
+alias fix-perm="sudo chown -R kevmer:domain^users"
+alias ll="ls -alrth"
+alias usb="cd `mount | grep cryptusb | awk '{print $3}'`"
+
+run_backup() {
+ USB=`mount | grep cryptusb | awk '{print $3}'`
+ rsync -av --delete-after /local/ubuntu-backup $USB/
+}
