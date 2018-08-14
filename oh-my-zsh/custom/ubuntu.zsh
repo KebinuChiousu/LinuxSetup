f97df6004a4fa63d6811c3124c1235acae6122f5
Kevin Meredith <kevin@meredithkm.info>
2018-08-14 16:51:15 -0400

    update ubuntu.zsh script.

--- oh-my-zsh/custom/ubuntu.zsh
+++ oh-my-zsh/custom/ubuntu.zsh
@@ -1,11 +1,11 @@
-alias tz='sudo dpkg-reconfigure tzdata'
-alias idg="id | tr ',' '\n'"
-alias du-nc="du --exclude=.cache -h"
-alias fix-perm="sudo chown -R kevmer:domain^users"
-alias ll="ls -alrth"
-alias usb="cd `mount | grep cryptusb | awk '{print $3}'`"
-
 run_backup() {
  USB=`mount | grep cryptusb | awk '{print $3}'`
  rsync -av --delete-after /local/ubuntu-backup $USB/
 }
+
+alias tz='sudo dpkg-reconfigure tzdata'
+alias idg="id | tr ',' '\n'"
+alias du-nc="du --exclude=.cache -h"
+alias fix-perm="sudo chown -R `id -un`:`id -gn`"
+alias ll="ls -alrth"
+alias usb="cd `mount | grep cryptusb | awk '{print $3}'`"
